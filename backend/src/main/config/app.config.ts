8712ff2ceb09efe5f0374aa5c41ad0589fa3fd96
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-19 19:37:36 -0300

    ♻️ make changes to support the new logger

--- backend/src/main/config/app.config.ts
+++ backend/src/main/config/app.config.ts
@@ -1,5 +1,12 @@
-import fastify, {FastifyInstance} from 'fastify'
+import fastify, {
+	FastifyInstance,
+	RawReplyDefaultExpression,
+	RawRequestDefaultExpression,
+	RawServerDefault
+} from 'fastify'
 import {RouteConfig} from './route.config'
+import {DefaultLogger} from './logger.config'
+import pino from 'pino'
 
 export const start = async () => {
 	const app = await load()
@@ -13,10 +20,22 @@ export const start = async () => {
 	}
 }
 
-const listen = async (app: FastifyInstance): Promise<string> => {
+const listen = async (app: FastifyInstance<
+	RawServerDefault,
+	RawRequestDefaultExpression,
+	RawReplyDefaultExpression,
+	pino.Logger<never>
+>): Promise<string> => {
 	return app.listen({ port: 3000 })
 }
 
-const load = async (): Promise<FastifyInstance> => {
-	return fastify()
+const load = async (): Promise<FastifyInstance<
+	RawServerDefault,
+	RawRequestDefaultExpression,
+	RawReplyDefaultExpression,
+	pino.Logger<never>
+>> => {
+	return fastify({
+		logger: DefaultLogger()
+	})
 }
--- backend/src/main/config/route.config.ts
+++ backend/src/main/config/route.config.ts
@@ -1,8 +1,19 @@
-import {FastifyInstance} from 'fastify'
+import {
+	FastifyInstance,
+	RawReplyDefaultExpression,
+	RawRequestDefaultExpression,
+	RawServerDefault
+} from 'fastify'
 import {QuestionRoutes} from '../../presentation/routes/question.route'
 import {Versioning} from './versioning.config'
+import pino from 'pino'
 
-export const RouteConfig = (app: FastifyInstance) => {
+export const RouteConfig = (app: FastifyInstance<
+	RawServerDefault,
+	RawRequestDefaultExpression,
+	RawReplyDefaultExpression,
+	pino.Logger<never>
+>) => {
 	app
 		.register(QuestionRoutes, { prefix: Versioning.V1 })
 }
