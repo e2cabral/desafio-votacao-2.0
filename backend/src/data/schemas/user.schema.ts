c008ec5f9759224fbb8815ff8a30ba32e86e81af
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 20:04:52 -0300

    🚧 add a new property password

--- backend/src/data/schemas/user.schema.ts
+++ backend/src/data/schemas/user.schema.ts
@@ -1,12 +1,22 @@
 import mongoose, {Schema} from 'mongoose'
 import {UserEntity} from '../../domain/entities/user.entity'
+import bcrypt from 'bcrypt'
 
 export const User = new Schema<UserEntity>({
 	name: { type: 'String', required: true },
 	email: { type: 'String', required: true },
+	password: { type: 'String', required: true, select: false },
 	phone: { type: 'String', required: true },
 	votes: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Voting', required: false }],
 	questions: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Question', required: false }]
+})
+
+User.pre('save', async function (next) {
+	if (this.isModified('password')) {
+		const salt = await bcrypt.genSalt(10)
+		this.password = await bcrypt.hash(this.password, salt)
+	}
+	next()
 })
 
 export const UserModel = mongoose.model('User', User)
--- backend/src/domain/entities/user.entity.ts
+++ backend/src/domain/entities/user.entity.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 export interface UserEntity {
     name: string
     email: string
+    password: string
     phone: string
     votes: mongoose.ObjectId[]
     questions: mongoose.ObjectId[]
