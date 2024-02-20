c80230ac268848ae411092eafc419a545d6602dc
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 14:28:26 -0300

    ✨ add a new route the retrieve just questions started

--- backend/src/presentation/routes/question.route.ts
+++ backend/src/presentation/routes/question.route.ts
@@ -1,16 +1,13 @@
 import {FastifyInstance, RouteShorthandOptions} from 'fastify'
 import { create } from '../controllers/question/create'
 import {startSession} from '../controllers/question/start-session'
+import {findStarted} from '../controllers/question/find-started'
 
 export const QuestionRoutes = (app: FastifyInstance, _: RouteShorthandOptions, done: () => void) => {
 	app
 		.post(
 			'/question',
-			{
-				schema: {
-					tags: ['Question']
-				}
-			},
+			{ schema: { tags: ['Question'] } },
 			create
 		)
 		.patch(
@@ -21,6 +18,19 @@ export const QuestionRoutes = (app: FastifyInstance, _: RouteShorthandOptions, d
 				}
 			},
 			startSession
+		)
+		.get(
+			'/question/started',
+			{
+				schema: {
+					querystring: {
+						page: { type: 'number' },
+						itemsPerPage: { type: 'number' },
+					},
+					tags: ['Question']
+				}
+			},
+			findStarted
 		)
 
 	done()
