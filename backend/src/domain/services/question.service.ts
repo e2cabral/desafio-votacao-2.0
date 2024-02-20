4e9d616d10b2d7f86043a5a84d45e35b4cd8a1ac
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 11:05:50 -0300

    🚧 create a service to hold the create function

---
+++ backend/src/domain/services/question.service.ts
@@ -0,0 +1,13 @@
+import QuestionEntity from '../entities/question.entity'
+import {Question} from '../../data/repositories/question/create'
+import { logger } from '../../main/config/logger.config'
+
+export default class QuestionService {
+	async create(question: QuestionEntity) {
+		try {
+			await Question.create(question)
+		} catch (err) {
+			logger.error((err as Error).message)
+		}
+	}
+}
