7d47e3163fde8bde4fd181b4bcd7fa68b62bc133
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 11:02:29 -0300

    ♻️ remove the question id

--- backend/src/data/schemas/voting.schema.ts
+++ backend/src/data/schemas/voting.schema.ts
@@ -3,7 +3,6 @@ import {VotingEntity} from '../../domain/entities/question.entity'
 
 export const Voting = new Schema<VotingEntity>({
 	userId: { type: mongoose.SchemaTypes.ObjectId, required: true },
-	questionId: { type: mongoose.SchemaTypes.ObjectId, required: true },
 	answer: { type: 'String', required: true }
 })
 
