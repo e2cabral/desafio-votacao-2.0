ce1ead3bb37642fae9be535ac20e3027f49acc0b
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 09:44:39 -0300

    ✨ add a call to load environment vars from .env file

--- backend/src/main/config/app.config.ts
+++ backend/src/main/config/app.config.ts
@@ -8,13 +8,19 @@ import {RouteConfig} from './route.config'
 import {DefaultLogger} from './logger.config'
 import pino from 'pino'
 import {DocumentationConfig} from './documentation.config'
+import Database from '../../infra/database'
+import {config} from 'dotenv'
 
 export const start = async () => {
 	const app = await load()
 
 	try {
+		config()
+
 		await DocumentationConfig(app)
 		await RouteConfig(app)
+
+		await Database.connect()
 
 		await listen(app)
 	} catch (err) {
