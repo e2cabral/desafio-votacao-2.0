8b74d53bc6d4d2bf1bdca14fecee46cbf7305ea4
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 17:43:28 -0300

    ✨ update the user with the question created

--- backend/src/data/repositories/question/index.ts
+++ backend/src/data/repositories/question/index.ts
@@ -1,11 +1,17 @@
 import {QuestionModel} from '../../schemas/question.schema'
 import QuestionEntity from '../../../domain/entities/question.entity'
 import { logger } from '../../../main/config/logger.config'
+import {UserModel} from '../../schemas/user.schema'
 
 export namespace Question {
 	export const create = async (question: QuestionEntity) => {
 		try {
-			return QuestionModel.create(question)
+			const questionCreated = await QuestionModel.create(question)
+			const user = await UserModel.findOne({ _id: question.createdBy })
+
+			await user!.updateOne({ $push: { questions: questionCreated._id } })
+
+			return questionCreated
 		} catch (err) {
 			logger.error((err as Error).message)
 		}
