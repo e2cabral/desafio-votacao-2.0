1c1e25b30fd2631dbb78000002a098f449bbb390
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-21 08:49:28 -0300

    ✨ create an auth service

--- backend/src/domain/services/auth.service.ts
+++ backend/src/domain/services/auth.service.ts
@@ -1,13 +1,14 @@
 import {Auth} from '../../data/repositories/auth'
 import {logger} from '../../main/config/logger.config'
-import {FastifyInstance} from 'fastify'
+import {FastifyRequest} from 'fastify'
 
 export default class AuthService {
-	async login(email: string, password: string, app: FastifyInstance) {
+	async login(email: string, password: string, request: FastifyRequest) {
 		try {
-			return Auth.login(email, password, app)
+			return Auth.login(email, password, request)
 		} catch (err) {
 			logger.error((err as Error).message)
+			throw err
 		}
 	}
 }
