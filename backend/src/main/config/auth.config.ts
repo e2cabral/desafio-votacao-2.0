c449ef67509c128ef87eb1dc83cbf63bcee184ce
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-21 08:45:31 -0300

    🚧 create hooks and a decorator to verify the authentication

--- backend/src/main/config/auth.config.ts
+++ backend/src/main/config/auth.config.ts
@@ -1,6 +1,14 @@
-import {FastifyInstance, RawReplyDefaultExpression, RawRequestDefaultExpression, RawServerDefault} from 'fastify'
+import {
+	FastifyInstance,
+	FastifyReply,
+	FastifyRequest,
+	RawReplyDefaultExpression,
+	RawRequestDefaultExpression,
+	RawServerDefault
+} from 'fastify'
 import pino from 'pino'
-import fastifyJwt from '@fastify/jwt'
+import fastifyJwt, {FastifyJWT} from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const AuthConfig = async (app: FastifyInstance<
     RawServerDefault,
@@ -9,5 +17,34 @@ export const AuthConfig = async (app: FastifyInstance<
     pino.Logger<never>
 >) => {
 	app
-		.register(fastifyJwt, { secret: String(process.env.JWT_SECRET) })
+		.register(
+			fastifyJwt,
+			{ secret: String(process.env.JWT_SECRET) }
+		)
+
+	app.addHook('onRequest', (request: FastifyRequest, reply: FastifyReply, next: () => void) => {
+		request.jwt = app.jwt
+		return next()
+	})
+
+	app.decorate(
+		'authenticate',
+		async (request: FastifyRequest, reply: FastifyReply) => {
+			const token = request.cookies.access_token
+
+			if (!token) {
+				return reply
+					.status(401)
+					.send({ message: 'Authentication required' })
+			}
+			request.user = request
+				.jwt
+				.verify<FastifyJWT['user']>(token)
+		}
+	)
+
+	app.register(fastifyCookie, {
+		secret: String(process.env.JWT_SECRET),
+		hook: 'onRequest',
+	})
 }
