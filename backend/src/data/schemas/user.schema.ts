934697e6d55ff8a69c608367851ca302ad9036dd
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-20 21:11:34 -0300

    🐛 remove select false from user schema

--- backend/src/data/schemas/user.schema.ts
+++ backend/src/data/schemas/user.schema.ts
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt'
 export const User = new Schema<UserEntity>({
 	name: { type: 'String', required: true },
 	email: { type: 'String', required: true },
-	password: { type: 'String', required: true, select: false },
+	password: { type: 'String', required: true },
 	phone: { type: 'String', required: true },
 	votes: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Voting', required: false }],
 	questions: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Question', required: false }]
