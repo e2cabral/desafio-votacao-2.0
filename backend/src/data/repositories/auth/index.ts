f4c08579b4c527c363f39e78aa13985696aa8dc8
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-21 09:07:51 -0300

    ✨ add a register function

--- backend/src/data/repositories/auth/index.ts
+++ backend/src/data/repositories/auth/index.ts
@@ -1,6 +1,8 @@
 import {UserModel} from '../../schemas/user.schema'
 import bcrypt from 'bcrypt'
 import {FastifyRequest} from 'fastify'
+import {UserEntity} from '../../../domain/entities/user.entity'
+import {logger} from '../../../main/config/logger.config'
 
 export namespace Auth {
 export const login = async (email: string, password: string, request: FastifyRequest) => {
@@ -17,5 +19,20 @@ export const login = async (email: string, password: string, request: FastifyReq
 	}
 
 	return request.jwt.sign({payload: { email: user.email, id: user._id }})
+}
+
+export const register = async (user: UserEntity) => {
+	try {
+		const isEmailRegistered = await UserModel.findOne({ email: user.email })
+		if (isEmailRegistered) {
+			throw new Error('Email already registered')
+		}
+
+		return UserModel.create(user)
+	} catch (err) {
+		const message = (err as Error).message
+		logger.error(message)
+		throw err
+	}
 }
 }
