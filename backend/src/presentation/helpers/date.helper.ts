9f0a0e458fd6ba29c37ae194d225dcb3a180f25a
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-23 13:30:56 -0300

    🚧 export leapYear function

--- backend/src/presentation/helpers/date.helper.ts
+++ backend/src/presentation/helpers/date.helper.ts
@@ -24,6 +24,6 @@ export class DaysPerMonth {
 	}
 }
 
-const isLeapYear = (year: number): boolean => {
+export const isLeapYear = (year: number): boolean => {
 	return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 }
