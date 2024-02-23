c38c3ede2bb029cc1cd59647c5aa65f7a264a923
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-23 11:46:57 -0300

    🚧 create a register and a verification if the user can vote in the service

--- backend/src/domain/services/voting/index.ts
+++ backend/src/domain/services/voting/index.ts
@@ -1,13 +1,41 @@
 import {VotingEntity} from '../../entities/question.entity'
 import {Voting} from '../../../data/repositories/voting'
 import {logger} from '../../../main/config/logger.config'
+import {VoterEntity} from '../../entities/voting.entity'
+import {validateCPF} from '../../../utils/cpf-validation.util'
 
 export namespace VotingService {
 	export const vote = async (questionId: string, voting: VotingEntity) => {
 		try {
 			return Voting.vote(questionId, voting)
 		} catch (err) {
-			logger.error((err as Error).message)
+			const message = (err as Error).message
+			logger.error(message)
+			throw err
+		}
+	}
+
+	export const registerVoter = async (voter: VoterEntity) => {
+		try {
+			const isCpfValid = validateCPF(voter.cpf)
+
+			if (!isCpfValid) throw new Error('CPF inválido')
+
+			return Voting.registerVoter(voter)
+		} catch (err) {
+			const message = (err as Error).message
+			logger.error(message)
+			throw err
+		}
+	}
+
+	export const canVote = async (cpf: string) => {
+		try {
+			return Voting.canVote(cpf)
+		} catch (err) {
+			const message = (err as Error).message
+			logger.error(message)
+			throw err
 		}
 	}
 }
