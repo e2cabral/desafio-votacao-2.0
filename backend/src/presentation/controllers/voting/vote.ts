dc8f1eea1f1697ef5988a50ef45c383aa9135518
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-23 11:47:12 -0300

    🚧 create a register and a verification if the user can vote in the controller

--- backend/src/presentation/controllers/voting/vote.ts
+++ backend/src/presentation/controllers/voting/vote.ts
@@ -3,6 +3,7 @@ import {ConvertTo} from '../../helpers/convertion.helper'
 import {VotingEntity} from '../../../domain/entities/question.entity'
 import {logger} from '../../../main/config/logger.config'
 import {VotingService} from '../../../domain/services/voting'
+import {VoterEntity} from '../../../domain/entities/voting.entity'
 
 export const vote = async (request: FastifyRequest, reply: FastifyReply) => {
 	try {
@@ -10,6 +11,34 @@ export const vote = async (request: FastifyRequest, reply: FastifyReply) => {
 		const { questionId } = ConvertTo<{ questionId: string }>(request.params)
 
 		const result = await VotingService.vote(questionId, voting)
+
+		reply.status(200).send({ body: result })
+	} catch (err) {
+		const message = (err as Error).message
+		logger.error(message)
+		reply.status(400).send({ body: message })
+	}
+}
+
+export const registerVoter = async (request: FastifyRequest, reply: FastifyReply) => {
+	try {
+		const voter = ConvertTo<VoterEntity>(request.body)
+
+		const result = await VotingService.registerVoter(voter)
+
+		reply.status(200).send({ body: result })
+	} catch (err) {
+		const message = (err as Error).message
+		logger.error(message)
+		reply.status(400).send({ body: message })
+	}
+}
+
+export const canVote = async (request: FastifyRequest, reply: FastifyReply) => {
+	try {
+		const { cpf } = ConvertTo<{ cpf: string }>(request.params)
+
+		const result = await VotingService.canVote(cpf)
 
 		reply.status(200).send({ body: result })
 	} catch (err) {
